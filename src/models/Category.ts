7f40af96cc9926a7d8ff5d7415be0ea986ca30db
Olavo Licursi <55329614+olavo-licursi@users.noreply.github.com>
2020-11-28 03:51:28 -0300

    Desafio Finalizado

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,20 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
